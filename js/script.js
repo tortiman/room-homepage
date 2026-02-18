3b4e137dee0356dbee636da27e5768a318132952
tortiman <josejavierbedmar@gmail.com>
2026-02-18 19:04:53 +0100

    Actualizar enlaces en el README y corregir rutas de imágenes en el script.js

--- js/script.js
+++ js/script.js
@@ -14,17 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const slides = [
         {
             title: "Discover innovative ways to decorate",
-            image: "../images/desktop-image-hero-1.jpg",
+            image: "desktop-image-hero-1.jpg",
             description:"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
         },
         {
             title: "We are available all across the globe",
-            image: "../images/desktop-image-hero-2.jpg",
+            image: "desktop-image-hero-2.jpg",
             description:"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
         },
         {
             title: "Manufactured with the best materials",
-            image: "../images/desktop-image-hero-3.jpg",
+            image: "desktop-image-hero-3.jpg",
             description:"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
 
         }   
